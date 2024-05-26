81882df0b1e662dc78487697f092cf1e693d2ded
amit1661 <yadavamit1661@gmail.com>
2024-05-26 10:55:02 +0530

    First commit counterApp

---
+++ script.js
@@ -0,0 +1,27 @@
+const countValue = document.getElementById("counter");
+
+function increment() {
+  // get the vlue from UI
+  let value = parseInt(countValue.innerText);
+  //Update the value
+  value = value + 1;
+  //set the value onto UI
+  countValue.innerText = value;
+}
+
+function decrement() {
+  // get the vlue from UI
+  let value = parseInt(countValue.innerText);
+  //Update the value
+  value = value - 1;
+  //set the value onto UI
+  countValue.innerText = value;
+}
+function reset() {
+  // get the vlue from UI
+  let value = parseInt(countValue.innerText);
+  //Update the value
+  value = 0;
+  //set the value onto UI
+  countValue.innerText = value;
+}
